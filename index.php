7c498c6f96c3c9c7fe6e2d65fc3cdfbd535cd9c2
NaidaSma <naida.smailbegovic@stu.ibu.edu.ba>
2022-04-27 04:26:01 +0200

    čl

--- index.php
+++ index.php
@@ -1,8 +1,15 @@
 <!DOCTYPE html!>
 <html>
+<head>
+	<title>Blood Donation Managment System</title>
+</head>
 <body>
 	<?php
 		echo "Hello World!";
+		echo "<hr>";
+		echo $_SERVER['PHP_SELF'];
+		echo "<hr>";
+		echo $_SERVER['SERVER_NAME'];
 	 ?>
  </body>
 </html>
