ff9ece7fe3712aa65973329260d5f87922a62af7
NaidaSma <naida.smailbegovic@stu.ibu.edu.ba>
2022-04-28 01:44:20 +0200

    sad

--- API/dao/Register.class.php
+++ API/dao/Register.class.php
@@ -1,8 +1,14 @@
 <!DOCTYPE html!>
 <html>
 <body>
-Name: <?php echo $_POST["name"]; ?><br>
-Surname: <?php echo $_POST["surname"]; ?><br>
-Birthday: <?php echo $_POST["birthday"];?><br>
+Name: <?php echo $_POST["name"]; ?>
+Surname: <?php echo $_POST["surname"]; ?>
+Date of birth: <?php echo $_POST["dateofBirth"];?>
+Address: <?php echo $_POST["address"]; ?>
+City: <?php echo $_POST["city"]; ?><br>
+Telephone number: <?php echo $_POST["telephone"]; ?>
+E-mail: <?php echo $_POST["email"]; ?>
+Password: <?php echo $_POST["password"]; ?>
+Blood type:<?php echo $_POST["bloodtype"]; ?>
 </body>
 </html>
--- API/dao/index.php
+++ API/dao/index.php
@@ -8,19 +8,35 @@
 </head>
 <body>
 	<?php
-		echo "Hello World!";
-		echo "<hr>";
 		echo $_SERVER['PHP_SELF'];
 		echo "<hr>";
 		echo $_SERVER['SERVER_NAME'];
 	 ?>
-	 <h4>Login</h4>
+	 <h4>Register</h4>
 	 <section id="register-form">
 		 <form method="POST"action="register.class.php">
 			 <input name="name" placeholder="Name">
-			 <input name="surname" placeholder="Surname">
-			 <input name="birthday" placeholder="Birthday"><br>
-			 <input type="submit">
+			 <input name="surname" placeholder="Surname"><br>
+			 <input name="dateofBirth" placeholder="Date of birth"><br>
+			 <h3>Gender</h3>
+			 <input type="radio" name="gender" value="Female"/>Female
+			 <input type="radio" name="gender" value="Male"/>Male
+			 <h3>Blood type</h3>
+			 <input type="checkbox" name="bloodtype"/>A+<br>
+			  <input type="checkbox" name="bloodtype"/>A-<br>
+				 <input type="checkbox" name="bloodtype"/>B+<br>
+				  <input type="checkbox" name="bloodtype"/>B-<br>
+					 <input type="checkbox" name="bloodtype"/>0+<br>
+					  <input type="checkbox" name="bloodtype"/>0-<br>
+						 <input type="checkbox" name="bloodtype"/>AB+<br>
+						  <input type="checkbox" name="bloodtype"/>AB-<br>
+			 <input name="address" placeholder="Address">
+			 <input name="city" placeholder="City"><br>
+				<input name="telephone" placeholder="Telephone number">
+				<input name="email" placeholder="E-mail"><br>
+				 <input name="password" placeholder="Password"><br>
+			 <input type="button" value="Register">
+
 		 </form>
 	 </section>
  </body>
